719293fb1934d3f163bedea029148f522df89862
hong <785131182@qq.com>
2020-04-28 21:10:01 +0800

    增加随机数生成

--- commonUtil/common.go
+++ commonUtil/common.go
@@ -1,6 +1,10 @@
 package commonUtil
 
-import "regexp"
+import (
+	"math/rand"
+	"regexp"
+	"time"
+)
 
 func CheckMailFormat(mail string) bool {
 	return regexp.MustCompile(`^[A-Za-z0-9]+([_\.][A-Za-z0-9]+)*@([A-Za-z0-9\-]+\.)+[A-Za-z]{2,6}$`).MatchString(mail)
@@ -8,4 +12,13 @@ func CheckMailFormat(mail string) bool {
 
 func CheckPhoneFormat(phone string) bool {
 	return regexp.MustCompile(`^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$`).MatchString(phone)
+}
+
+//return rand num between [min, max]
+func RandBetween(min, max int) int {
+	if min >= max {
+		panic("min bigger than max")
+	}
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min+1) + min
 }
