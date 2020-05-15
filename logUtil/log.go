2f53336bdf267bc18af55d384ef4157435000240
hong <785131182@qq.com>
2020-05-15 13:51:25 +0800

    fix

--- logUtil/log.go
+++ logUtil/log.go
@@ -15,17 +15,20 @@ const (
 	colorYellow = 33
 	colorBlue   = 34
 
-	dTypeStd  = 0
-	dTypeFile = 1
-
 	levelT = "[T] "
 	levelE = "[E] "
 	levelW = "[W] "
 
-	fileSize       = 60 * 1024 * 1024
-	logPath        = "log/"
-	defaultLogName = "default.log"
+	defaultFileSize = 60 * 1024 * 1024
+	minFileSize     = 1 * 1024 * 1024
+	defaultLogDir   = "log"
+	defaultLogName  = "default.log"
+
+	logTypeStd logType = iota + 1
+	logTypeFile
 )
+
+type logType int
 
 var (
 	defaultLogger = &myLog{}
@@ -33,101 +36,134 @@ var (
 
 type myLog struct {
 	sync.Once
-	logChan    chan *data //日志写通道
-	fileWriter *os.File   //输出到文件
-	stdWriter  io.Writer  //输出到控制台
+	sync.Mutex
+	outs     map[logType]io.Writer //writer集合
+	file     *os.File              //文件句柄
+	fileName string                //日志名
+	dir      string                //日志存放路径
+	size     int64                 //单个日志文件的大小限制
 }
 
-type data struct {
-	dType   uint8
-	content string
+type LogOption func(log *myLog)
+
+func WithSize(size int64) LogOption {
+	return func(log *myLog) {
+		log.size = size
+	}
 }
 
-func InitLogger() {
+func WithLogDir(dir string) LogOption {
+	return func(log *myLog) {
+		log.dir = dir
+	}
+}
+
+func WithFileName(name string) LogOption {
+	return func(log *myLog) {
+		log.fileName = name
+	}
+}
+
+func InitLogger(args ...LogOption) {
 	defaultLogger.Do(func() {
-		defaultLogger = new(myLog)
-		defaultLogger.logChan = make(chan *data, 1024)
+		for _, af := range args {
+			af(defaultLogger)
+		}
 		defaultLogger.init()
 	})
 }
 
 func (m *myLog) init() {
-	if !isExist(logPath) {
-		if err := os.Mkdir(logPath, 0777); err != nil {
+	if m.dir == "" {
+		m.dir = defaultLogDir
+	}
+	if m.fileName == "" {
+		m.fileName = defaultLogName
+	}
+	if m.size == 0 {
+		m.size = defaultFileSize
+	} else {
+		if m.size < minFileSize {
+			panic(fmt.Sprintf("invalid size: %d", m.size))
+		}
+	}
+
+	if m.outs == nil {
+		m.outs = make(map[logType]io.Writer)
+	}
+	if !isExist(m.dir) {
+		if err := os.Mkdir(m.dir, 0777); err != nil {
 			panic(err)
 		}
 	}
-	fileName := logPath + defaultLogName
-	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	file, err := os.OpenFile(m.dir+"/"+m.fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
 	if err != nil {
 		panic(err)
 	}
-	m.fileWriter = file
-	m.stdWriter = os.Stdout
-	go m.writeLog()
-}
 
-func (m *myLog) writeLog() {
-	if m.logChan == nil {
-		return
-	}
-	for data := range m.logChan {
-		if data == nil {
-			continue
-		}
-		m.checkLogSize()
-		if data.dType == dTypeStd {
-			fmt.Fprintf(m.stdWriter, data.content+"\n")
-		}
-		if data.dType == dTypeFile {
-			fmt.Fprintf(m.fileWriter, data.content+"\n")
-		}
-	}
+	m.file = file
+	m.outs[logTypeStd] = os.Stdout
+	m.outs[logTypeFile] = file
 }
 
 func (m *myLog) checkLogSize() {
-	if m.fileWriter == nil {
+	if m.file == nil {
 		return
 	}
-	fileInfo, err := m.fileWriter.Stat()
+	m.Lock()
+	defer m.Unlock()
+	fileInfo, err := m.file.Stat()
 	if err != nil {
 		panic(err)
 	}
-	if fileSize > fileInfo.Size() {
+	if m.size > fileInfo.Size() {
 		return
 	}
 	//需要分割
-	name := logPath + time.Now().Format("2006_01_02_15:04:03") + ".log"
-	m.fileWriter.Close()
-
-	err = os.Rename(logPath+defaultLogName, name)
+	name := time.Now().Format("2006_01_02_15:04:03") + ".log"
+	err = os.Rename(m.dir+"/"+m.fileName, m.dir+"/"+name)
 	if err != nil {
 		panic(err)
 	}
 
-	file, err := os.OpenFile(logPath+defaultLogName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	file, err := os.OpenFile(m.dir+"/"+m.fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
 	if err != nil {
 		panic(err)
 	}
-	m.fileWriter = file
+
+	m.file.Close()
+	m.file = file
+	m.outs[logTypeFile] = file
 	return
+}
+
+func (m *myLog) write(level string, content string) {
+	m.checkLogSize()
+	var colorText int
+	switch level {
+	case levelT:
+		colorText = colorBlue
+	case levelW:
+		colorText = colorYellow
+	case levelE:
+		colorText = colorRed
+	}
+
+	for k, wr := range m.outs {
+		if k == logTypeStd {
+			fmt.Fprintf(wr, setColor(content, colorText))
+		} else {
+			fmt.Fprintf(wr, content)
+		}
+	}
 }
 
 ////Info
 func T(format string, v ...interface{}) {
 	_, file, line, _ := runtime.Caller(1)
 	codeLine := "[" + shortFileName(file) + ":" + strconv.Itoa(line) + "]"
-	content := levelT + codeLine + fmt.Sprintf(format, v...)
-	var d = &data{
-		dType:   dTypeStd,
-		content: setColor(content, colorBlue),
-	}
-	defaultLogger.logChan <- d
-	var d1 = &data{
-		dType:   dTypeFile,
-		content: content,
-	}
-	defaultLogger.logChan <- d1
+	content := levelT + codeLine + fmt.Sprintf(format, v...) + "\n"
+	defaultLogger.write(levelT, content)
 }
 
 //
@@ -135,36 +171,16 @@ func T(format string, v ...interface{}) {
 func E(format string, v ...interface{}) {
 	_, file, line, _ := runtime.Caller(1)
 	codeLine := "[" + shortFileName(file) + ":" + strconv.Itoa(line) + "]"
-	content := levelE + codeLine + fmt.Sprintf(format, v...)
-
-	var d = &data{
-		dType:   dTypeStd,
-		content: setColor(content, colorRed),
-	}
-	defaultLogger.logChan <- d
-	var d1 = &data{
-		dType:   dTypeFile,
-		content: content,
-	}
-	defaultLogger.logChan <- d1
+	content := levelE + codeLine + fmt.Sprintf(format, v...) + "\n"
+	defaultLogger.write(levelE, content)
 }
 
 //Warn
 func W(format string, v ...interface{}) {
 	_, file, line, _ := runtime.Caller(1)
 	codeLine := "[" + shortFileName(file) + ":" + strconv.Itoa(line) + "]"
-	content := levelW + codeLine + fmt.Sprintf(format, v...)
-
-	var d = &data{
-		dType:   dTypeStd,
-		content: setColor(content, colorYellow),
-	}
-	defaultLogger.logChan <- d
-	var d1 = &data{
-		dType:   dTypeFile,
-		content: content,
-	}
-	defaultLogger.logChan <- d1
+	content := levelW + codeLine + fmt.Sprintf(format, v...) + "\n"
+	defaultLogger.write(levelW, content)
 }
 
 func isExist(path string) bool {
