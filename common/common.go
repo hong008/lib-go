8162d7b5bbc88611e8f3c2a677423dc1b6bdca65
hong <785131182@qq.com>
2020-12-29 12:17:20 +0800

    add type.go

--- common/common.go
+++ common/common.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"encoding/base64"
 	"math/rand"
 	"os"
 	"os/signal"
@@ -10,6 +11,9 @@ import (
 	"syscall"
 	"time"
 	"unsafe"
+
+	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/scrypt"
 )
 
 const (
@@ -25,22 +29,56 @@ var (
 	phoneChecker = regexp.MustCompile(`^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$`)
 )
 
+type Tool interface {
+	//校验邮箱格式
+	CheckMailFormat(mail string) bool
+	//校验电话号码格式
+	CheckPhoneFormat(phone string) bool
+	//在min和max之间生成一个随机数
+	RandBetween(min, max int) int
+	//内部随机生成一个数，判断是否小于per
+	LessThanIn100(per int) bool
+	//[]byte转string
+	Bytes2String(bytes []byte) string
+	//string转[]byte
+	String2Bytes(s string) []byte
+	//如果监听到系统中断信号，则执行onNotify()
+	Notify(onNotify func())
+	//随机字符串
+	RandString(n int) string
+	//src是否包含ele
+	Contain(src interface{}, ele interface{}) bool
+	//bcrypt算法加密明文密钥
+	BcEncryptPass(plainPass string) (string, error)
+	//bcrypt算法比较明文和密文密钥
+	BcComparePass(hashPass, plainPass string) error
+	//scrypt算法加密明文密钥
+	ScryptPass(plainPass, salt string) (string, error)
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+type tool struct {
+}
+
+func NewTool() Tool {
+	return new(tool)
+}
+
 //校验邮箱格式
-func CheckMailFormat(mail string) bool {
+func (t *tool) CheckMailFormat(mail string) bool {
 	return mailChecker.MatchString(mail)
 }
 
 //校验电话号码格式
-func CheckPhoneFormat(phone string) bool {
+func (t *tool) CheckPhoneFormat(phone string) bool {
 	return phoneChecker.MatchString(phone)
 }
 
 //生成min-max之间的一个随机数
-func RandBetween(min, max int) int {
+func (t *tool) RandBetween(min, max int) int {
 	if min >= max {
 		panic("min bigger than max")
 	}
@@ -48,26 +86,26 @@ func RandBetween(min, max int) int {
 }
 
 //生成一个1-100的随机数, 用于简单的判断概率
-func LessThanIn100(per int) bool {
+func (t *tool) LessThanIn100(per int) bool {
 	if per < 1 || per > 100 {
 		panic("input must between 1 and 100")
 	}
-	return per >= RandBetween(1, 100)
+	return per >= t.RandBetween(1, 100)
 }
 
 //[]byte convert to string
-func Bytes2String(bytes []byte) string {
+func (t *tool) Bytes2String(bytes []byte) string {
 	return *(*string)(unsafe.Pointer(&bytes))
 }
 
 //string convert to []byte
-func String2Bytes(s string) []byte {
+func (t *tool) String2Bytes(s string) []byte {
 	sp := *(*[2]uintptr)(unsafe.Pointer(&s))
 	bp := [3]uintptr{sp[0], sp[1], sp[1]}
 	return *(*[]byte)(unsafe.Pointer(&bp))
 }
 
-func Notify(onNotify func()) {
+func (t *tool) Notify(onNotify func()) {
 	//SIGHUP		终端控制进程结束(终端连接断开)
 	//SIGQUIT		用户发送QUIT字符(Ctrl+/)触发
 	//SIGTERM		结束程序(可以被捕获、阻塞或忽略)
@@ -90,7 +128,7 @@ func Notify(onNotify func()) {
 }
 
 //随机指定长度的字符串
-func RandString(n int) string {
+func (t *tool) RandString(n int) string {
 	if n <= 0 {
 		panic("invalid n.")
 	}
@@ -112,7 +150,7 @@ func RandString(n int) string {
 }
 
 //判断src中是否有元素ele
-func Contain(src interface{}, ele interface{}) bool {
+func (t *tool) Contain(src interface{}, ele interface{}) bool {
 	switch reflect.TypeOf(src).Kind() {
 	case reflect.Slice:
 		s := reflect.ValueOf(src)
@@ -125,50 +163,31 @@ func Contain(src interface{}, ele interface{}) bool {
 	return false
 }
 
-func Bool(b bool) *bool {
-	var ptr = new(bool)
-	*ptr = b
-	return ptr
+//用于加密web的密码
+func (t *tool) BcEncryptPass(plainPass string) (string, error) {
+	var encryptPass string
+	data, err := bcrypt.GenerateFromPassword([]byte(plainPass), bcrypt.DefaultCost)
+	if err != nil {
+		return encryptPass, err
+	}
+	encryptPass = base64.StdEncoding.EncodeToString(data)
+	return encryptPass, nil
 }
 
-func String(s string) *string {
-	var ptr = new(string)
-	*ptr = s
-	return ptr
+//比较密码是否匹配
+func (t *tool) BcComparePass(hashPass, plainPass string) error {
+	hashBytes, err := base64.StdEncoding.DecodeString(hashPass)
+	if err != nil {
+		return err
+	}
+	return bcrypt.CompareHashAndPassword(hashBytes, []byte(plainPass))
 }
 
-func Byte(b byte) *byte {
-	var ptr = new(byte)
-	*ptr = b
-	return ptr
-}
-
-func Int(i int) *int {
-	var ptr = new(int)
-	*ptr = i
-	return ptr
-}
-
-func Int32(i int32) *int32 {
-	var ptr = new(int32)
-	*ptr = i
-	return ptr
-}
-
-func Int64(i int64) *int64 {
-	var ptr = new(int64)
-	*ptr = i
-	return ptr
-}
-
-func Float32(f float32) *float32 {
-	var ptr = new(float32)
-	*ptr = f
-	return ptr
-}
-
-func Float64(f float64) *float64 {
-	var ptr = new(float64)
-	*ptr = f
-	return ptr
+func (t *tool) ScryptPass(plainPass, salt string) (string, error) {
+	data, err := scrypt.Key([]byte(plainPass), []byte(salt), 1<<15, 8, 1, 32)
+	if err != nil {
+		return "", err
+	}
+	encryptPass := base64.StdEncoding.EncodeToString(data)
+	return encryptPass, nil
 }
