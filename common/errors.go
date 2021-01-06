85c7805d81392e4767ea29795111a2781b8d7425
hong <785131182@qq.com>
2021-01-06 15:56:16 +0800

    add error,math

---
+++ common/errors.go
@@ -0,0 +1,44 @@
+package common
+
+import (
+	"fmt"
+)
+
+type Error interface {
+	Error() string
+	Code() int
+	Message() string
+}
+
+type myError struct {
+	code    int
+	message string
+}
+
+func New(code int, message string) Error {
+	return &myError{
+		code:    code,
+		message: message,
+	}
+}
+
+func (m *myError) Error() string {
+	if m == nil {
+		return ""
+	}
+	return fmt.Sprintf("Code: %d, Message: %s", m.code, m.message)
+}
+
+func (m *myError) Code() int {
+	if m != nil {
+		return m.code
+	}
+	return 0
+}
+
+func (m *myError) Message() string {
+	if m != nil {
+		return m.message
+	}
+	return ""
+}
---
+++ common/math.go
@@ -0,0 +1,36 @@
+package common
+
+func MaxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func MinInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func MaxInt32(a, b int32) int32 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func MinInt32(a, b int32) int32 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
