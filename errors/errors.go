52b5f3a2cd656712d6cf41080fb4d3386ca5599a
hong <785131182@qq.com>
2021-02-26 20:21:17 +0800

    error

--- errors/errors.go
+++ errors/errors.go
@@ -12,6 +12,8 @@ type Error interface {
 	Error() string
 	Code() int
 	Message() string
+	WithCode(code int) Error
+	WithMessage(msg string) Error
 }
 
 type myError struct {
@@ -26,6 +28,29 @@ func NewError(code int, message string) Error {
 	}
 }
 
+func Errorf(m string, args ...interface{}) Error {
+	e := &myError{}
+	e.message = fmt.Sprintf(m, args...)
+
+	return e
+}
+
+func (m *myError) WithCode(code int) Error {
+	if m == nil {
+		return nil
+	}
+	m.code = code
+	return m
+}
+
+func (m *myError) WithMessage(msg string) Error {
+	if m == nil {
+		return nil
+	}
+	m.message = msg
+	return m
+}
+
 func (m *myError) Error() string {
 	if m == nil {
 		return ""
@@ -33,16 +58,16 @@ func (m *myError) Error() string {
 	return fmt.Sprintf("Code: %d, Message: %s", m.code, m.message)
 }
 
+func (m *myError) Message() string {
+	if m == nil {
+		return ""
+	}
+	return m.message
+}
+
 func (m *myError) Code() int {
 	if m != nil {
 		return m.code
 	}
 	return 0
-}
-
-func (m *myError) Message() string {
-	if m != nil {
-		return m.message
-	}
-	return ""
 }
