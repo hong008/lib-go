32a8d182f08ca29d6efa0dfd8b32390ca3efcdc5
hong <785131182@qq.com>
2021-03-02 17:53:33 +0800

    file

--- files/file.go
+++ files/file.go
@@ -2,7 +2,7 @@ package files
 
 import "os"
 
-//判断目录是否存在，如果不存在，则新建一个目录
+// MakeNewPath 判断目录是否存在，如果不存在，则新建一个目录
 func MakeNewPath(targetPath string) error {
 	if _, err := os.Stat(targetPath); err != nil {
 		if !os.IsExist(err) {
